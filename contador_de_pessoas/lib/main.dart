03063c362c41d6719e5d1b30c24dacf8c91d1499
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-04-07 13:46:19 -0300

    Adicionada imagem de fundo e identação de código para yaml

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -4,50 +4,61 @@ void main() {
   runApp(
     MaterialApp(
       title: "Contador de pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text(
-            "Pessoas: 0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          Image.asset(
+            "images/restaurante.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
           ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                child: Text(
-                  "+1",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40.0,
-                      fontStyle: FontStyle.italic),
-                ),
-                onPressed: () {},
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontStyle: FontStyle.italic),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontStyle: FontStyle.italic),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-              
-             Padding(
-                padding: EdgeInsets.all(10.0),
-                child: FlatButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40.0,
-                      fontStyle: FontStyle.italic),
-                ),
-                onPressed: () {},
-              ),
-              ),
+              Text(
+                "Entrada liberada",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 25),
+              )
             ],
           ),
-          Text("Entrada liberada",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 25))
         ],
       ),
     ),
